04d7160f40429c71f3a5363f6b9fb523d9d2bf2a
aliffrhn <alif.farhanw@gmail.com>
2020-03-09 21:02:16 +0700

    add caption search

--- feed.php
+++ feed.php
@@ -61,8 +61,56 @@
     </nav>
     <main id="feed">
         <?php
-        echo isset($_GET['caption']);
-        while($row = mysqli_fetch_assoc($result)){?>
+        if(!isset($_GET['caption'])){
+            while($row = mysqli_fetch_assoc($result)){?>
+                <div class="photo">
+                    <header class="photo__header">
+                        <img src="images/avatar.jpg" class="photo__avatar" />
+                        <div class="photo__user-info">
+                            <span class="photo__author">inthetiger</span>
+                            <span class="photo__location">Bestechung</span>
+                        </div>
+                    </header>
+                    <img src="<?php echo $row['url'] ?>" />
+                    <div class="photo__info">
+                        <div class="photo__actions">
+                            <span class="photo__action">
+                                <i class="fa fa-heart-o fa-lg"></i>
+                            </span>
+                            <span class="photo__action">
+                                <i class="fa fa-comment-o fa-lg"></i>
+                            </span>
+                        </div>
+                        <span class="photo__likes"><?php echo $row['likes'] ?> likes</span>
+                        <div class="photo__caption"><?php echo $row['caption'] ?></div><br>
+                        <ul class="photo__comments">
+                            <li class="photo__comment">
+                                <span class="photo__comment-author">serranoarevalo</span> love this!
+                            </li>
+                            <li class="photo__comment">
+                                <span class="photo__comment-author">serranoarevalo</span> love this!
+                            </li>
+                            <li class="photo__comment">
+                                <span class="photo__comment-author">serranoarevalo</span> love this!
+                            </li>
+                            <li class="photo__comment">
+                                <span class="photo__comment-author">serranoarevalo</span> love this!
+                            </li>
+                        </ul>
+                        <span class="photo__time-ago">2 hours ago</span>
+                        <div class="photo__add-comment-container">
+                            <textarea name="comment" placeholder="Add a comment..."></textarea>
+                            <i class="fa fa-ellipsis-h"></i>
+                        </div>
+                    </div>
+                </div>
+        <?php }}
+        else{
+            $caption = $_GET['caption'];
+            $sql = "SELECT * FROM photo WHERE caption like'%$caption%'";
+            $result = $conn->query($sql);
+            while($row = mysqli_fetch_assoc($result)){ 
+        ?>
             <div class="photo">
                 <header class="photo__header">
                     <img src="images/avatar.jpg" class="photo__avatar" />
@@ -104,7 +152,8 @@
                     </div>
                 </div>
             </div>
-        <?php } ?>
+    <?php }}
+    ?>
     </main>
     <footer class="footer">
         <div class="footer__column">
