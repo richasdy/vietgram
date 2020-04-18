29f5291889a0a451395005762b67634961fe781c
aliffrhn <alif.farhanw@gmail.com>
2020-04-18 22:59:39 +0700

    webci

--- filter.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('koneksi.php');
-    $search = $_POST['caption'];
-    $sql = "SELECT * FROM photo WHERE caption like '$search'";
-    $result = $conn->query($sql);
-    // $row = $result->fetch_assoc();
-?>
