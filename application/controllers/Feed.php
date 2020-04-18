76008eb44d213a21a08eee2c3a2c86fffb58affc
Safa Salsabila Damara <salsa.pinter@gmail.com>
2020-04-19 02:05:46 +0800

    tubes webpro

---
+++ application/controllers/Feed.php
@@ -0,0 +1,20 @@
+<?php
+
+class Feed extends CI_Controller {
+
+    function __construct(){
+        parent::__construct();
+        $this->load->model('user');
+        $this->load->model('post');
+    }
+
+    public function index() {
+        $username = $this->session->userdata('user');
+        if ($username) {
+            $data['allPost'] = $this->post->getAllPost();
+            $this->load->view('view_feed',$data);
+        } else {
+            redirect('/login');
+        }
+    }
+}
